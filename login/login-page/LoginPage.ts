44a1d7ddad676800e0629ebd1014f1fbcafe57ad
quan2903 <thaianhquan690@gmail.com>
2025-09-26 16:22:17 +0700

    fix lỗi undefined username - password

--- login/login-page/LoginPage.ts
+++ login/login-page/LoginPage.ts
@@ -1,5 +1,9 @@
 import {Page} from 'playwright';
 
+if (!process.env.ADMIN_USERNAME || !process.env.ADMIN_PASSWORD) {
+  require('dotenv').config(); 
+}
+
 export class LoginPage {
   
   constructor(public page: Page) {
@@ -11,8 +15,9 @@ export class LoginPage {
     });
   }
     async login(username: string, password: string) {
+        await this.page.getByRole('textbox', { name: 'Username' }).click();
         await this.page.getByRole('textbox', { name: 'Username' }).fill(username);
-
+        await this.page.getByRole('textbox', { name: 'Password' }).click();
         await this.page.getByRole('textbox', { name: 'Password' }).fill(password);
         await this.page.getByRole('button', { name: 'Login' }).press('Enter');
     }
